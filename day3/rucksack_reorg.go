2630f72e79905eed457c9baed3cdd80b12d7e373
Julia Kordick <moin@jkordick.dev>
2022-12-03 10:20:03 +0100

    Added day 3 part 2

--- day3/rucksack_reorg.go
+++ day3/rucksack_reorg.go
@@ -9,6 +9,7 @@ import (
 
 var priority_map = make(map[string]int)
 var priority_value int
+var group_priotity_value int
 
 func main() {
 	input_data, err := ioutil.ReadFile("input.txt")
@@ -21,14 +22,27 @@ func main() {
 
 	setupPriorityMap()
 
-	for i := 0; i < len(input_data_string_array); i++ {
-		firstHalf, secondHalf := halveString(input_data_string_array[i])
+	findPriotityValue(input_data_string_array)
+	findGroupPriorityValue(input_data_string_array)
+
+}
+
+func findGroupPriorityValue(str []string) {
+	for i := 0; i < len(str); i += 3 {
+		group_priority_char := findPriotityChar(str[i], str[i+1], str[i+2])
+		group_priotity_value += assignPriority(group_priority_char)
+	}
+
+	fmt.Println("summed up group priorities:", group_priotity_value)
+}
+
+func findPriotityValue(str []string) {
+	for i := 0; i < len(str); i++ {
+		firstHalf, secondHalf := halveString(str[i])
 		priority_char := compareChars(firstHalf, secondHalf)
 		priority_value += assignPriority(priority_char)
 	}
-
 	fmt.Println("summed up priorities:", priority_value)
-
 }
 
 func halveString(str string) (string, string) {
@@ -48,6 +62,19 @@ func compareChars(str1, str2 string) string {
 
 func assignPriority(char string) int {
 	return priority_map[char]
+}
+
+func findPriotityChar(str1, str2, str3 string) string {
+	for _, char1 := range str1 {
+		for _, char2 := range str2 {
+			for _, char3 := range str3 {
+				if string(char1) == string(char2) && string(char2) == string(char3) {
+					return string(char1)
+				}
+			}
+		}
+	}
+	return ""
 }
 
 // a - z -> 1 - 26, A - Z -> 27 - 52
